ad0307a49c99f4fac18b44473bfd6bffc012309c
Adiesha Liyanage <adiesha@gmail.com>
2020-11-12 18:50:25 -0700

    testing on annoy

--- graphtest.py
+++ graphtest.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import networkx as nx
+from annoy import AnnoyIndex
 
 
 def main():
@@ -12,6 +13,28 @@ def main():
     for i in nx.connected_components(G):
         print(type(i))
         print(i)
+
+    data = pd.read_csv('data/shuttle-unsupervised-trn.csv', header=None)
+    t = AnnoyIndex(9, 'euclidean')
+    for i in data.index:
+        t.add_item(i, data.iloc[i, 0:9])
+
+    t.build(10)
+    res = t.get_nns_by_item(2, 6, include_distances=False)
+    res2 = t.get_nns_by_item(2, 6, include_distances=True)
+    a, b = res2 = t.get_nns_by_item(2, 6, include_distances=True)
+    print(res)
+    print(res2)
+    res2[1].remove(res2[0].index(2))
+    res2[0].remove(2)
+    print(res2)
+    eps = 1.8
+    mapelementinradius = list(map(lambda x: x >= eps, res2[1]))
+    aaaa = np.array(list(map(lambda x: x >= eps, res2[1])))
+    print(aaaa)
+    print(mapelementinradius)
+    elementindeicesinradius = (np.array(res2[0]))[mapelementinradius]
+    print(elementindeicesinradius)
 
 
 if __name__ == '__main__':
